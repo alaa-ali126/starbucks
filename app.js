53abad3ab26e6b00332871cfeb71664e0020f4ae
alaa <alaa.ali61220@gmail.com>
2023-08-19 12:05:14 +0300

    v1.1

---
+++ app.js
@@ -0,0 +1,7 @@
+function changeThumb(imgslider) {
+  document.querySelector(".cup img").src = imgslider;
+}
+
+function changeColor(color) {
+  document.querySelector(".circle").style.backgroundColor = color;
+}
